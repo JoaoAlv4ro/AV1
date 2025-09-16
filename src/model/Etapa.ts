bf3019e46ae19217001f187033f3443376e4f01b
João Álvaro <joaosilvaabc1@gmail.com>
2025-09-16 11:47:25 -0300

    ✨ feat: Métodos Etapa (Iniciar, Finalizar e Funcionários)

--- src/model/Etapa.ts
+++ src/model/Etapa.ts
@@ -14,4 +14,46 @@ export default class Etapa {
         this.status = StatusEtapa.PENDENTE;
         this.funcionarios = [];
     }
+
+    public iniciarEtapa(): void {
+        if (this.status === StatusEtapa.PENDENTE) {
+            this.status = StatusEtapa.ANDAMENTO;
+            console.log(`Etapa ${this.nome} Iniciada`);
+        } else {
+            console.log(`Ação não permitida: A etapa ${this.nome} não está pendente.`);
+        }
+    }
+
+    public finalizarEtapa(): void {
+        if (this.status === StatusEtapa.ANDAMENTO) {
+            this.status = StatusEtapa.CONCLUIDA;
+            console.log(`Etapa ${this.nome} Iniciada`);
+        } else {
+            console.log(`Ação não permitida: A etapa ${this.nome} não está em andamento.`);
+        }
+    }
+
+    public associarFuncionario(funcionario: Funcionario): void {
+        const funcionarioExiste = this.funcionarios.find(f => f.id === funcionario.id);
+
+        if (funcionarioExiste) {
+            console.log(`Funcionário ${funcionario.nome} já está associado à etapa ${this.nome}.`);
+        } else {
+            this.funcionarios.push(funcionario);
+            console.log(`Funcionário ${funcionario.nome} associado à etapa ${this.nome}.`);
+        }
+    }
+
+    public listarFuncionarios(): Funcionario[] {
+        console.log(`---- Lista de Funcionários - Etapa: ${this.nome}` );
+        if (this.funcionarios.length === 0) {
+            console.log("Nenhum funcionário associado a esta etapa.");
+            return [];
+        }
+
+        this.funcionarios.forEach(f => {
+            console.log(`${f.id + 1}: ${f.nome}`);
+        });
+        return this.funcionarios
+    }
 }
